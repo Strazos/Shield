34446e0daa8ad61d83d2d30dcb9a8493af91061e
Glitch (shield) <none>
2018-07-25 01:02:37 +0000

    🖼📊 Updated with Glitch

--- bot.js
+++ bot.js
@@ -7,6 +7,18 @@
 const DBM = {};
 const DiscordJS = DBM.DiscordJS = require('discord.js');
 
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+console.log(Date.now() + " Ping Received");
+response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+http.get(`http://${process.env.shield}.glitch.me/`);
+}, 280000);
+
 //---------------------------------------------------------------------
 // Bot
 // Contains functions for controlling the bot.
